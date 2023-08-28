03caa030fdccc5d37b7b762e75d4a045ef8d2143
Arthur <arfure21@gmail.com>
2023-08-28 14:23:07 -0300

    Projeto no intelliJ utilizando o framework Quarkus construindo um CRUD

---
+++ src/main/java/controller/CustomerController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import entity.Customer;
+import jakarta.ws.rs.core.Response;
+import org.jboss.resteasy.reactive.RestPath;
+import service.CustomerService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/api/customer")
+public class CustomerController {
+
+    @Inject
+    CustomerService customerService;
+
+    @GET
+    public List<Customer> retrieveCustomers(){
+        List<Customer> customers = new ArrayList<>();
+        try{
+            customers = customerService.findAllCustomers();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return customers;
+    }
+
+    @POST
+    @Transactional
+    public void addCustomer(Customer customer){
+        customerService.addCustomer(customer);
+        Response.ok().status(201);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateCustomer(@PathParam("id") Long id, Customer customer){
+        return customerService.updateCustomer(customer, id);
+
+    }
+
+    @DELETE
+    @Transactional
+    public void deleteCustomer(Customer customer){
+        customerService.deleteCustomer(customer);
+    }
+
+}
---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,47 @@
+package service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import entity.Customer;
+import jakarta.ws.rs.core.Response;
+import repository.CustomerRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class CustomerService {
+
+    @Inject
+    CustomerRepository customerRepository;
+
+
+    public List<Customer> findAllCustomers(){
+        return customerRepository.findAll().list();
+    }
+
+    public void addCustomer(Customer customer){
+        customerRepository.persist(customer);
+    }
+    public Response updateCustomer(Customer customer, Long id) {
+
+        Customer customerObj = customerRepository.findById(id);
+
+
+        if(customerObj != null){
+            customerObj.setName(customer.getName());
+            customerObj.setLastName(customer.getLastName());
+            customerObj.setAge(customer.getAge());
+            customerObj.setEmail(customer.getEmail());
+
+        } else{
+            return  Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.status(Response.Status.ACCEPTED).build();
+
+    }
+    public void deleteCustomer(Customer customer){
+        customerRepository.delete(customer);
+    }
+}
